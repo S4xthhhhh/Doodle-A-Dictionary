00b6d6e2349fd70c22c1aad91d9f7be8e7a9b4ce
Sangeeth S Devan <sangeethsdevan@gmail.com>
2024-01-25 21:06:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function btn_clicked(){
+    const word =document.getElementById("input-search").value;
+    getMeaning(word)
+}
+
+async function getMeaning(word){
+    let data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    let response = await data.json()
+    document.getElementById("result").style.display="block";
+    document.getElementById("input-word").textContent=word
+    document.getElementById("phonetic").textContent=response[0].phonetic
+
+    document.getElementById("POS").textContent=response[0].meanings[0].partOfSpeech
+    document.getElementById("Meaning").textContent=response[0].meanings[0].definitions[0].definition
+    document.getElementById("Example").textContent=response[0].meanings[0].definitions[0].example
+}

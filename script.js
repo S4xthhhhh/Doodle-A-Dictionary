423430c76043843714d5663ea0bed8b6c003414a
Sangeeth S Devan <sangeethsdevan@gmail.com>
2024-01-26 21:21:59 +0530

    update

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ function btn_clicked(){
     document.getElementById("POS").textContent='';
     document.getElementById("Meaning").textContent='';
     document.getElementById("Example").textContent="";
-    document.getElementById("error").style.display="none"
     getMeaning(word)
 }
 
 async function getMeaning(word){
     let data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     let response = await data.json()
-     if(response[0]?.meanings[0]?.partOfSpeech){
+
+    if(response[0]?.meanings[0]?.partOfSpeech){
         document.getElementById("result").style.display="block";
+        document.getElementById("error").style.display="none"
         document.getElementById("input-word").textContent=word
         document.getElementById("phonetic").textContent=response[0].phonetic
         document.getElementById("POS").textContent=response[0].meanings[0].partOfSpeech
-        document.getElementById("Meaning").textContent=response[0].meanings[0].definitions[0].definition
         document.getElementById("Example").textContent=response[0].meanings[0].definitions[0].example
+
+
+        const defArray=response[0].meanings[0].definitions
+        defArray.forEach((value)=>{
+        document.getElementById("Meaning").insertAdjacentHTML('afterend', '<li>'+value.definition+'</li>')
+        })
+
+        if(response[0]?.meanings[1]?.definitions[0]){
+            document.getElementById("s-POS").textContent=response[0].meanings[1].partOfSpeech
+            document.getElementById("s-Example").textContent=response[0].meanings[1].definitions[0].example
+            const defArraytwo=response[0].meanings[1].definitions
+            defArraytwo.forEach((value)=>{
+            document.getElementById("s-Meaning").insertAdjacentHTML('afterend', '<li>'+value.definition+'</li>')
+        })
+        }
     }else{
         document.getElementById("error").style.display="block"
-
-document.getElementById("result").style.display="none";
+        document.getElementById("result").style.display="none";
     }
 }

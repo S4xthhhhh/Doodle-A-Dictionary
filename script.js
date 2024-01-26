5634959d83acf2e8e70fecfe0da77ba987f4df9e
Sangeeth S Devan <117325461+S4xthhhhh@users.noreply.github.com>
2024-01-26 17:11:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ function btn_clicked(){
 async function getMeaning(word){
     let data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     let response = await data.json()
-    document.getElementById("result").style.display="block";
-    document.getElementById("input-word").textContent=word
-    document.getElementById("phonetic").textContent=response[0].phonetic
-
-    document.getElementById("POS").textContent=response[0].meanings[0].partOfSpeech
-    document.getElementById("Meaning").textContent=response[0].meanings[0].definitions[0].definition
-    document.getElementById("Example").textContent=response[0].meanings[0].definitions[0].example
+     if(response[0]?.meanings[0]?.partOfSpeech){
+        console.log("OK")
+        document.getElementById("result").style.display="block";
+        document.getElementById("input-word").textContent=word
+        document.getElementById("phonetic").textContent=response[0].phonetic
+        document.getElementById("POS").textContent=response[0].meanings[0].partOfSpeech
+        document.getElementById("Meaning").textContent=response[0].meanings[0].definitions[0].definition
+        document.getElementById("Example").textContent=response[0].meanings[0].definitions[0].example
+    }else{
+        document.getElementById("error").style.display="block"
+    }
 }
